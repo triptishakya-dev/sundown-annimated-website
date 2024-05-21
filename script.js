02f0a002556fead00b72ef7c5b842925cc122ceb
triptishakya-dev <triptishakya.2001@gmail.com>
2024-05-21 23:21:46 +0530

    website completed

--- script.js
+++ script.js
@@ -46,6 +46,34 @@ function swiperAnimation() {
         
     });
 }
-
+function menuAnimation(){
+    var menu = document.querySelector("nav h3")
+  var full =  document.querySelector("#full-scr")
+  var naving = document.querySelector("nav img")
+  var flag = 0
+ menu.addEventListener("click" , function() {
+    if(flag== 0){
+    full.style.top = 0
+    naving.style.opacity = 0
+    flag = 1
+    }else{
+        full.style.top = "-100%"
+    naving.style.opacity = 1
+    flag = 0
+    }
+ })
+ }
+ function loaderAnimation(){
+    var loader =  document.querySelector("#loader")
+setTimeout(function(){
+    loader.style.top = "-100%"
+},4200)
+ }
 swiperAnimation()
 page4Animation()
+menuAnimation()
+loaderAnimation()
+
+
+ 
+ 
